afe97e6ea1a68501513667b9741cc6400271bb81
TTtie <me@tttie.cz>
2021-12-16 20:10:56 +0000

    add license

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Včela
 Tento plugin (který běžel primárně na BungeeCordu) zajišťoval na BeeGames následující věci:
 - Sbírání celkového počtu hráčů na serveru za účelem statistiky
+- Omezení přístupu dle státu/IP
 - Alerty na serveru, globální nebo jen na určitý server
 - Nahlašování reportů/hráčů
 - (nikdy nebylo spuštěno, ale je to tam) automatické globální zprávy po X minutách
+
+# Licence
+Protože Včela řešila některé důležité funkce serveru BeeGames, rozhodl jsem se ji zveřejnit pod [licencí GNU AGPLv3, nebo jejími pozdějšími verzemi](LICENSE).
--- src/main/kotlin/eu/beegames/core/common/db/DatabaseGetter.kt
+++ src/main/kotlin/eu/beegames/core/common/db/DatabaseGetter.kt
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2021 TTtie (BeeGames.eu)
+ * 
+ * This file is part of Včela.
+ * 
+ * Včela is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Včela is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Včela.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package eu.beegames.core.common.db
 
 import com.zaxxer.hikari.HikariDataSource
